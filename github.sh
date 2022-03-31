0fcb2f5ad1f59d6e4f83cb1e12d851971f585a5d
技术部-陈琦 <2881894929@qq.com>
2022-03-31 10:56:32 +0800

    完善

--- github.sh
+++ github.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'github仓库'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/zuimengxinbie/zuimengxinbie.github.io.git main
+git push https://github.com/zuimengxinbie/zuimengxinbie.github.io.git/ main
 
 cd -
